9e9b9f12504c0f261c2a78647985afc38772d986
yadneshchavhan <yadneshbug@gmail.com>
2026-01-20 01:46:15 -0800

    Vulnerable.sol

--- reentrancy/Vulnerable.sol
+++ reentrancy/Vulnerable.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ReentrancyVulnerable {
+    mapping(address => uint256) public balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 bal = balances[msg.sender];
+        require(bal > 0, "no balance");
+
+        (bool ok, ) = msg.sender.call{value: bal}("");
+        require(ok, "send failed");
+
+        balances[msg.sender] = 0;
+    }
+}
 
